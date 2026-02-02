64e82423302f13e2f9f4a84b21921ddce56f268d
unknown <rupalisarcar@gmail.com>
2026-02-03 01:06:07 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,34 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { setName,setEmail } from "../redux/userInfoSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const {name, email} = useSelector((state) => state.UserInfo);
+
+  const handleNameChange = (e) =>{
+    dispatch(setName(e.target.value))
+  }
+
+  const handleEmailChange = (e) =>{
+    dispatch(setEmail(e.target.value))
+  }
+
   return (
     <div>
+      <h1>User Information</h1>
+      <div>
+        <input type="text" value={name} placeholder="Enter name" onChange={handleNameChange}/>
+        <input type="email" placeholder="Enter email" onChange={handleEmailChange}/>
+      </div>
+    
+      <div style={{marginTop:'30px'}}>
+        <h2>Live Display</h2>
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
         {/* Do not remove the main div */}
     </div>
   )
---
+++ src/redux/userInfoSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userInfoSlice = createSlice({
+    name:'UserInfo',
+    initialState:{
+        name:'',
+        email:''
+    },
+    reducers:{
+        setName(state,action){
+            state.name = action.payload
+        },
+        setEmail(state,action){
+            state.email = action.payload
+        }
+    }
+})
+
+export const {setName, setEmail} = userInfoSlice.actions;
+export default userInfoSlice.reducer
